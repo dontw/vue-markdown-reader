b630f6b7c2da1664a251e68853e1bb1b711432ab
Chen-yu Hsieh <chenyuhsieh@starlux-airlines.com>
2018-08-10 09:45:58 +0800

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="container">
+    <div>
+      <app-logo/>
+      <div class="wrap">
+          <Button>Default</Button>
+          <Button type="primary">Primary</Button>
+          <Button type="dashed">Dashed</Button>
+          <Button type="text">Text</Button>
+          <br><br>
+          <Button type="info">Info</Button>
+          <Button type="success">Success</Button>
+          <Button type="warning">Warning</Button>
+          <Button type="error">Error</Button>
+      </div>
+
+    </div>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue';
+
+export default {
+  components: {
+    AppLogo,
+  },
+  methods: {},
+};
+</script>
+
+<style lang="less" scoped>
+.title {
+  background: linear-gradient(#e66465, #9198e5);
+}
+</style>
+
+
---
+++ plugins/iview.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import iView from 'iview';
+import en from 'iview/dist/locale/en-US';
+Vue.use(iView, { locale: en });
